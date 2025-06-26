a69b81ca1c927a364d8128cfdafaa2c07135029e
Ayrton Yoshii <itiro_yoshii@hotmail.com>
2025-06-26 15:42:32 -0300

    Exercicio 9

--- src/main/java/com/treinamento/erp/repository/AlunoRepository.java
+++ src/main/java/com/treinamento/erp/repository/AlunoRepository.java
@@ -28,11 +28,11 @@ public class AlunoRepository {
     }
 
     public void popularDadosIniciais(){
-        alunos.add(new Aluno(1L, "Lucas Ferreira", "123.456.789-12", newDate(1999, 12, 2), CursoNome.ADS));
-        alunos.add(new Aluno(2L, "Mariana Silva", "987.654.321-32", newDate(1995, 5, 20), CursoNome.CDC));
-        alunos.add(new Aluno(3L, "Pedro Costa", "456.789.123-41", newDate(2000, 1, 11), CursoNome.ENG));
-        alunos.add(new Aluno(4L, "Ana Beatriz", "321.654.987-84", newDate(2001, 4, 21), CursoNome.MED));
-        alunos.add(new Aluno(5L, "Carlos Oliveira", "159.753.486-29", newDate(1998, 7, 15), CursoNome.PSI));
+        alunos.add(new Aluno(1L, "Lucas Ferreira", "779.602.190-95", newDate(1999, 12, 2), CursoNome.ADS));
+        alunos.add(new Aluno(2L, "Mariana Silva", "997.033.110-83", newDate(1995, 5, 20), CursoNome.CDC));
+        alunos.add(new Aluno(3L, "Pedro Costa", "613.495.400-48", newDate(2000, 1, 11), CursoNome.ENG));
+        alunos.add(new Aluno(4L, "Ana Beatriz", "427.025.600-16", newDate(2001, 4, 21), CursoNome.MED));
+        alunos.add(new Aluno(5L, "Carlos Oliveira", "335.157.980-21", newDate(1998, 7, 15), CursoNome.PSI));
     }
 
     public List<Aluno> getDadosAlunos() {
